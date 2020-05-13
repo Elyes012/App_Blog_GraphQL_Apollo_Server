d2faf15ffc5e2a50ed782aaf016e722dfd0ef392
Elyes Maamouri <maamouri.ilyes@gmail.com>
2020-05-13 19:15:35 +0200

    Front-End : config semantic-ui & router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
+// Config module router
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
+import'semantic-ui-css/semantic.min.css';
+//Components
+import MenuBar from './components/MenuBar';
+// Pages
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-     <div><h1>Hello world !!</h1></div>
+    <Router>
+      <Container>
+      <MenuBar/>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component = {Login}/>
+      <Route exact path='/register' component = {Register}/>
+      </Container>
+     
+      </Router>
   );
 }
 
